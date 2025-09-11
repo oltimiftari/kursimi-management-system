763c7d03c7c9d4f94d8e8886a0dce790c151dcc7
OltiM4 <oltimiftarii2@gmail.com>
2025-09-11 19:57:13 +0200

    Backend Category API

---
+++ src/main/java/in/oltimiftari/kursimi/dto/CategoryDTO.java
@@ -0,0 +1,27 @@
+package in.oltimiftari.kursimi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class CategoryDTO {
+
+    private Long id;
+    private Long profileId;
+    private String name;
+    private String icon;
+    private String type;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
