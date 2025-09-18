7fb0a43979b382bdd74843e99a2e11e745d7f536
OltiM4 <oltimiftarii2@gmail.com>
2025-09-18 02:22:18 +0200

    Frontend Category Module

--- kursimiwebapp/src/pages/Category.jsx
+++ kursimiwebapp/src/pages/Category.jsx
@@ -1,12 +1,73 @@
 import Dashboard from "../components/Dashboard.jsx";
 import {useUser} from "../hooks/useUser.jsx";
+import {Plus} from "lucide-react";
+import CategoryList from "../components/CategoryList.jsx";
+import {useEffect, useState} from "react";
+import {API_ENDPOINTS} from "../util/apiEndpoints.js";
+import axiosConfig from "../util/axiosConfig.jsx";
+import Modal from "../components/Modal.jsx";
 
 const Category = () => {
+
     useUser();
+    const [loading, setLoading] = useState(false);
+    const [categoryData, setCategoryData] = useState([]);
+    const [openAddCategoryModal, setOpenAddCategoryModal] = useState(false);
+    const [openEditCategoryModal, setOpenEditCategoryModal] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    const fetchCategoryDetails = async () => {
+        if(loading) return;
+
+        setLoading(true);
+
+        try {
+            const response = await axiosConfig.get(API_ENDPOINTS.GET_ALL_CATEGORIES);
+            if (response.status === 200) {
+                console.log('categories',response.data);
+                setCategoryData(response.data);
+            }
+        }catch(error) {
+            console.error('Something went wrong. Please try again.', error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchCategoryDetails();
+    }, []);
 
     return (
         <Dashboard activeMenu="Category">
-            This is Category page
+            <div className="my-5 mx-auto">
+                {/* Add button to add category*/}
+                <div className="flex justify-between items-center mb-5">
+                    <h2 className="text-2xl font-semibold">All Categories</h2>
+                    <button
+                        onClick={() => setOpenAddCategoryModal(true)}
+                        className="add-btn flex items-center gap-1">
+                        <Plus size={15} />
+                        Add Category
+                    </button>
+                </div>
+
+                {/* Category list */}
+                <CategoryList categories={categoryData} />
+
+                {/* Adding category modal*/}
+                <Modal
+                    isOpen={openAddCategoryModal}
+                    onClose={() => setOpenAddCategoryModal(false)}
+                    title="Add Category"
+                >
+                    Category form
+                </Modal>
+
+                {/* Updating category modal*/}
+
+            </div>
         </Dashboard>
     )
 }
